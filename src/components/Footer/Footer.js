f88c7c3bf8138569e10e05a4d376e0c8dab95020
Andrea Leon <andrealeonfandino@gmail.com>
2022-08-22 03:47:10 -0300

    RedacTeaca

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
         <>
             <footer>
-                <section className="container">
-                    <h1>RedacTeca</h1>
-                    <span>&copy;{new Date().getFullYear()} Copyright 2022 - RedacTeca</span>
-                    <span>Todos los derechos reservados</span>
+                <section>
+                    <img src="https://res.cloudinary.com/andrealeonfandino/image/upload/v1658416498/samples/redacteca/book-2028349_1280_qykxed.png" alt="logo"/>
+                    <span>&copy; {new Date().getFullYear()} RedacTeca. Todos los derechos reservados</span>
+                    <span>RedacTeca es un servicio de pago, su contenido está sujeto a disponibilidad. El servicio es comercializado por Andrea León (Argentina).</span>
                 </section>
             </footer>
         </>
